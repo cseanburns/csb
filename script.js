b50cc59677253a0613b86ba393067ec7e28255ca
C. Sean Burns <sean.csb@gmail.com>
2017-11-17 14:49:14 -0500

    commented out script in progress

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+/*
+
 function timeOfDayColors() {
   if (new Date().getHours() > 18)
   var bg = document.querySelector("body");
   bg.style.backgroundColor = "#696969";
   bg.style.fontColor = "#FFF";
 }
+
+*/
