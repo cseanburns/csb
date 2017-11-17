6d4e7182e2a00400810eae4f2e304d9426cc1474
C. Sean Burns <sean.csb@gmail.com>
2017-11-17 14:45:16 -0500

    worthless updates

---
+++ script.js
@@ -0,0 +1,6 @@
+function timeOfDayColors() {
+  if (new Date().getHours() > 18)
+  var bg = document.querySelector("body");
+  bg.style.backgroundColor = "#696969";
+  bg.style.fontColor = "#FFF";
+}
