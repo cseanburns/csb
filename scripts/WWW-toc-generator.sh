63e442ddf6897a69f6439600321f1c51daf2d739
cseanburns <sean.csb@gmail.com>
2020-04-02 09:33:40 -0400

    updated json+ld and toc generator

--- scripts/WWW-toc-generator.sh
+++ scripts/WWW-toc-generator.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Date: 2020-03-27
-# This script generates a list of entries from WWW/index.html and
+# This script generates a list of entries from WWW/index.html (the blog) and
 # creates an HTML list of them for entry into ~/index.html
+#
+# To generate the toc:
+# Usage: ./WWW-toc-generator.sh ../WWW/index.html > tmp.html 
    
 grep "h2 id=" $1 | sed -e 's/h2 id/a href/' -e\
 's/h2/a/' -e\
