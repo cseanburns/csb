d283b9e7a6d3bf7d77dcc3469bd2e86e1585a917
cseanburns <sean.csb@gmail.com>
2020-04-12 15:31:50 -0400

    updated script - most recent entry

--- scripts/WWW-toc-generator.sh
+++ scripts/WWW-toc-generator.sh
@@ -7,8 +7,19 @@
 # To generate the toc:
 # Usage: ./WWW-toc-generator.sh ../WWW/index.html > tmp.html 
    
+# Create list item from other HTML tags
 grep "h2 id=" $1 | sed -e 's/h2 id/a href/' -e\
 's/h2/a/' -e\
 's/\"/\"WWW\/index.html\#/' -e\
 's/^/\<li\>/' -e\
-'s/$/\<\/li\>/'
+'s/$/\<\/li\>/' > tmp.html
+
+# Get the most recent entry only; comment out if want the entire list
+head -n1 tmp.html > tmp1.html
+
+# Cleanup
+trash tmp.html
+
+# Automatically add to ../index.html
+# example code to work out later
+#sed -e "s/Two/Two\nThree/" test1.txt 
